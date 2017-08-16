be8fa87acba7b8342f27b96083f5959a9ea5db97
Laszlo Csele <lacka@codingsans.com>
2017-08-16 15:19:17 +0200

    added action buttons to call the native plugin

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, NgZone } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnDestroy {
+  result: { x: number, y: number, z: number } = { x: 0, y: 0, z: 0 };
 
-  constructor(public navCtrl: NavController) {
+  top = 0;
+  left = 0;
+  color = 'gray';
 
+  constructor(public navCtrl: NavController, private zone: NgZone) {
+    window['cordova']['plugins']['sensorManager']['initialize']();
+  }
+
+  start() {
+    window['cordova']['plugins']['sensorManager']['watch']((result) => {
+      console.log(result);
+      this.result = result;
+
+
+      const red = Math.floor(((result.x + 1) * 128) - 1);
+      const green = Math.floor(((result.y + 1) * 128) - 1);
+      const blue = Math.floor(((result.z + 1) * 128) - 1);
+
+      this.zone.run(() => {
+        this.left += result.x * 25;
+        this.top -= result.y * 25;
+        this.color = `rgb(${red}, ${green}, ${blue})`;
+      });
+    }, (err) => {
+      console.log(err);
+    }, 100);
+  }
+
+  stop() {
+    window['cordova']['plugins']['sensorManager']['finish']();
+  }
+
+  ngOnDestroy() {
+    this.stop();
   }
 
 }
