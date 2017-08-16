b83675219b12705d53263f070ee96b2f2b99c849
Laszlo Csele <lacka@codingsans.com>
2017-08-16 17:03:29 +0200

    circle init fix

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,35 +1,53 @@
-import { Component, OnDestroy, NgZone } from '@angular/core';
+import { Component, AfterViewInit, OnDestroy, NgZone, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage implements OnDestroy {
-  result: { x: number, y: number, z: number } = { x: 0, y: 0, z: 0 };
-
+export class HomePage implements AfterViewInit, OnDestroy {
+  @ViewChild('canvas') canvas;
+  ballWidth = 30;
   top = 0;
   left = 0;
   color = 'gray';
+  visible = false;
 
-  constructor(public navCtrl: NavController, private zone: NgZone) {
+  canvasWidth = 0;
+  canvasHeight = 0;
+
+  constructor(public navCtrl: NavController, private zone: NgZone) {}
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.canvasWidth = this.canvas.contentWidth;
+      this.canvasHeight = this.canvas.contentHeight;
+      this.top = (this.canvasHeight - this.ballWidth) / 2;
+      this.left = (this.canvasWidth - this.ballWidth) / 2;
+      this.visible = true;
+    }, 1000);
     window['cordova']['plugins']['sensorManager']['initialize']();
   }
 
   start() {
     window['cordova']['plugins']['sensorManager']['watch']((result) => {
-      console.log(result);
-      this.result = result;
-
-
-      const red = Math.floor(((result.x + 1) * 128) - 1);
-      const green = Math.floor(((result.y + 1) * 128) - 1);
-      const blue = Math.floor(((result.z + 1) * 128) - 1);
-
       this.zone.run(() => {
-        this.left += result.x * 25;
-        this.top -= result.y * 25;
-        this.color = `rgb(${red}, ${green}, ${blue})`;
+        let left = this.left - result.x * 15;
+        if (left < 0) {
+          left = 0;
+        }
+        if (left > this.canvasWidth - this.ballWidth) {
+          left = this.canvasWidth - this.ballWidth;
+        }
+        let top = this.top + result.y * 15;
+        if (top < 0) {
+          top = 0;
+        }
+        if (top > this.canvasHeight - this.ballWidth) {
+          top = this.canvasHeight - this.ballWidth;
+        }
+        this.left = left;
+        this.top = top;
       });
     }, (err) => {
       console.log(err);
@@ -43,5 +61,4 @@ export class HomePage implements OnDestroy {
   ngOnDestroy() {
     this.stop();
   }
-
 }
